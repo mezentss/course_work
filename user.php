37b800b510f0f186145d9d1354d6e38e87c25e40
mezentss <118174457+mezentss@users.noreply.github.com>
2024-01-07 10:51:04 +0300

    07.01

--- user.php
+++ user.php
@@ -4,6 +4,7 @@ require("db_connect.php");
 require("category_selector.php");
 
 $user_id = $session_user['id'];
+$sugarLevelInfo = [];
 
 $user_query = "SELECT * FROM users WHERE id = {$user_id}";
 $user_result = mysqli_query($conn, $user_query);
@@ -21,6 +22,32 @@ $likes_un_query = "SELECT * FROM likes JOIN categories ON likes.unloved = catego
 $likes_un_result = mysqli_query($conn, $likes_un_query);
 $likes_un = mysqli_fetch_assoc($likes_un_result);
 
+$sugarLevels = [
+    "low" => [
+        "image" => "images\low_sugar_image.jpg",
+        "text" => "Ваш уровень сахара низкий",
+        "url" => "low_sugar_advice.php" 
+    ],
+    "medium" => [
+        "image" => "images\medium_sugar_image.jpg",
+        "text" => "Ваш уровень сахара в пределах нормы",
+        "url" => "medium_sugar_advice.php" 
+    ],
+    "high" => [
+        "image" => "images\high_sugar_image.jpg",
+        "text" => "Ваш уровень сахара высокий",
+        "url" => "high_sugar_advice.php" 
+    ]
+];
+
+if($sugar['sugar_level'] < 5.4) {
+    $sugarLevelInfo = $sugarLevels['low'];
+} elseif ($sugar['sugar_level'] >= 5.4 && $sugar['sugar_level'] <= 8.4) {
+    $sugarLevelInfo = $sugarLevels['medium'];
+} else {
+    $sugarLevelInfo = $sugarLevels['high'];
+}
+
 if(isset($_POST['update_profile'])) {
     $name = $_POST["name"];
     $password = $_POST["password"];
@@ -32,7 +59,7 @@ if(isset($_POST['update_profile'])) {
     $new_sugar_level = $_POST["sugar_level"];
     $insert_sugar_query = "UPDATE current_sugar SET sugar_level = '$new_sugar_level', time = NOW() WHERE user_id = $user_id";
     mysqli_query($conn, $insert_sugar_query);
-
+    
     if($new_sugar_level < 5.4) {
         $result = mysqli_query($conn, "CALL dbscan(0.2, 2)");
     } elseif ($new_sugar_level >= 5.4 && $new_sugar_level <= 8.4) {
@@ -52,56 +79,62 @@ if(isset($_POST['update_likes'])) {
 }
 
 $title = "Личный кабинет";
-$content = "
 
-<form method=\"POST\">
-    <div>
-        <label>ФИО</label>
-        <input type=\"text\" name=\"name\" value=\"{$user['name']}\" required>
+$content = '
+<div style="display: flex;">
+    <div style="padding: 0 20px 0 20px;">
+        <form method="POST">
+            <h2>Личные данные</h2>
+            <div>
+                <label>ФИО</label>
+                <input type="text" name="name" value= '.$user['name'].' required>
+            </div>
+            <div>
+                <label>Пароль</label>
+                <input type="password" name="password" required>
+            </div>
+            <div>
+                <button type="submit" name="update_profile">Сохранить</button>
+            </div>
+        </form>
+        <form method="POST">
+            <h2>Уровень сахара</h2>
+            <div>
+                <input type="number" name="sugar_level" value='.$sugar['sugar_level'].' step="0.1" required>
+            </div>
+            <div>
+                <button type="submit" name="update_sugar_level">Сохранить</button>
+            </div>
+        </form>
     </div>
-    
-    <div>
-        <label>Пароль</label>
-        <input type=\"password\" name=\"password\" required>
+    <div style="padding-left: 0 20px 0 20px;">
+        <form method="POST">
+            <h2>Категории продуктов</h2>
+            <div>
+                <label>Любимая категория:</label>
+                <select name="favourite_category">
+                    <option value= ""> '.$likes_fv['name']. '</option>
+                    ' . getCategoryOptions($conn) . '
+                </select>
+            </div>
+            <div>
+                <label>Нелюбимая категория:</label>
+                <select name="unloved_category">
+                    <option value= ""> '.$likes_un['name']. '</option>
+                    ' . getCategoryOptions($conn) . '
+                </select>
+            </div>
+            <div>
+                <button type="submit" name="update_likes">Сохранить предпочтения</button>
+            </div>
+        </form>
     </div>
-    
-    <div>
-        <button type=\"submit\" name=\"update_profile\">Сохранить</button>
+    <div style=" padding: 40px 0 0 450px;">
+    <img src="' . $sugarLevelInfo['image'] . '" alt="Уровень сахара" style="max-width: 200px;">
+    <p>' . $sugarLevelInfo['text'] . '</p>
+    <a href="' . $sugarLevelInfo['url'] . '" class="button">Получить совет</a>
     </div>
-</form>
-
-<form method=\"POST\">
-    <div>
-        <label for=\"sugar_level\">Уровень сахара</label>
-        <input type=\"number\" name=\"sugar_level\" value=\"$sugar[sugar_level]\" step=\"0.1\" required>
-    </div>
-    
-    <div>
-        <button type=\"submit\" name=\"update_sugar_level\">Сохранить</button>
-    </div>
-</form>
-";
-
-$content .= '
-<form method="POST">
-    <h3>Любимые и нелюбимые категории продуктов</h3>
-    <div>
-        <label>Любимая категория:</label>
-        <select name="favourite_category">
-            <option value= ""> '.$likes_fv['name']. '</option>
-            ' . getCategoryOptions($conn) . '
-        </select>
-    </div>
-    <div>
-        <label>Нелюбимая категория:</label>
-        <select name="unloved_category">
-        <option value= ""> '.$likes_un['name']. '</option>
-            ' . getCategoryOptions($conn) . '
-        </select>
-    </div>
-    <div>
-        <button type="submit" name="update_likes">Сохранить предпочтения</button>
-    </div>
+</div>
 </form>';
 
 require("template.php");
